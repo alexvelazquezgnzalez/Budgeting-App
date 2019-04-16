606a3f5cbe7286cdafcd306de5f9ba81be806d28
alexvelazquezgnzalez <alexv41748@gmail.com>
2019-04-15 17:19:18 -0700

    Created a budgeting class

--- Budgeting.js
+++ Budgeting.js
@@ -1,10 +1,27 @@
 var income = document.getElementById("Income");
 var incomeButton = document.getElementById("addIncome");
 let totalIncome = " ";
-
+let parentIncome = document.getElementById("totalIncome");
+let editIncome = document.createElement('button');
 function displayIncome() {
     totalIncome = income.value ;
+    parentIncome.textContent = totalIncome;
+    parentIncome.appendChild(editIncome).textContent = "Edit Income";
 }
 
 incomeButton.addEventListener('click', displayIncome);
 
+class budgetingApp {
+    constructor(){
+    }
+    storeUserIncome(){
+
+    }
+    storeUserPurchases(){
+
+    }
+    userDonate(){
+
+    }
+
+}
